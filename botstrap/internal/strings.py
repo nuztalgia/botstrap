2b21f09de9280f377d1ca9679b19dc5249793831
Nuz / Lovegood <nuztalgia@gmail.com>
2022-08-06 19:22:38 -0700

    Add docstrings for Strings class/methods and edit some default values.

--- botstrap/internal/strings.py
+++ botstrap/internal/strings.py
@@ -7,15 +7,65 @@ from typing import Any, Callable, Iterable, overload
 
 @dataclass(eq=False, frozen=True, kw_only=True)
 class Strings:
+    """A `dataclass` specifying the strings to be used by the Botstrap-provided CLI.
+
+    The attributes of this class are strings, `string.Template` objects, and tuples of
+    strings. Collectively, they determine the text that is displayed in the console when
+    you run a script that utilizes Botstrap.
+
+    Preconfigured string setups are provided by this class's `default()` and `compact()`
+    methods. If you desire further customization, you can create a new instance of this
+    class and specify any strings you'd like to change. All constructor arguments are
+    keyword-only.
+
+    Example:
+        >>> from botstrap import Botstrap, Strings
+        >>> from string import Template
+        >>>
+        >>> bot_strings = Strings(
+        >>>     discord_login_attempt=Template("Logging in with '$token_label' token."),
+        >>>     discord_login_success=Template(
+        >>>         "$bot_identifier reporting for duty in $token_label mode!"
+        >>>     ),
+        >>> )
+        >>> Botstrap(strings=bot_strings).run_bot()
+
+        $ python bot.py
+
+        bot: Logging in with 'default' token.
+        bot: BasicBot#1234 reporting for duty in default mode!
+    """
+
+    @classmethod
+    def default(cls) -> Strings:
+        """Returns an instance of this class with default values for all attributes.
+
+        The default strings are all in English and include ample vertical spacing (e.g.
+        extra newlines between sections of text) for ease of reading.
+        """
+        return cls()
+
+    @classmethod
+    def compact(cls) -> Strings:
+        """Returns an instance of this class with vertical spacing minimized.
+
+        In other words, the semantic contents of all attributes are the same as they are
+        in the `default()` collection of strings, but any newline characters are either
+        removed (for newlines at the beginning and end of a string) or replaced by a
+        space (for newlines in the middle of a string).
+        """
+        default_items = asdict(cls.default()).items()
+        return cls(**{key: _get_compact_value(value) for key, value in default_items})
+
     bot_token_prompt: str = "BOT TOKEN"
     bot_token_mismatch: Template = Template(
         "Decrypted keyfile data for $token_label doesn't look like a bot token.\n"
     )
     bot_token_missing: Template = Template(
-        "Keyfile for $token_label token doesn't exist.\n"
+        "Keyfile for $token_label bot token doesn't exist.\n"
     )
     bot_token_missing_add: Template = Template(
-        "You currently don't have a saved bot token for $token_label."
+        "You currently don't have a saved $token_label bot token."
         "\nWould you like to add one now?"
     )
     bot_token_creation_cue: str = (
@@ -77,7 +127,7 @@ class Strings:
     )
     cli_desc_info_specifier: Template = Template("in $token_label mode")
     cli_desc_token_id: Template = Template(
-        "The ID of the bot token to use for log-in: $token_ids"
+        "The ID of the token to use to run the bot ($token_ids)."
     )
     cli_desc_manage_tokens: str = "View/manage your saved Discord bot tokens."
     cli_desc_version: str = "Display the currently installed version."
@@ -91,7 +141,7 @@ class Strings:
     discord_login_failure: str = (
         "Failed to log in. Make sure your bot token is configured properly."
     )
-    exit_keyboard_interrupt: str = "\nReceived a keyboard interrupt."
+    exit_keyboard_interrupt: str = "\n\nReceived a keyboard interrupt."
     exit_user_choice: str = "\nReceived a non-affirmative response."
     exit_notice: str = "Exiting process.\n"
 
@@ -100,6 +150,33 @@ class Strings:
         format_response: Callable[[str], str] | None = None,
         quote_responses: bool = True,
     ) -> str:
+        """Returns a string prompting the user for an affirmative response.
+
+        Responses considered "affirmative" are defined by the `affirmation_responses`
+        attribute of an instance of this class, which is a tuple that consists of the
+        strings "yes" and "y" by default.
+
+        Example:
+            >>> from botstrap import Strings
+            >>> pig_latin = lambda text: f"{text[1:]}{text[0]}ay"
+            >>> Strings.default().get_affirmation_prompt(format_response=pig_latin)
+            'If so, type "esyay" or "yay"'
+
+        Args:
+            format_response:
+                A function that takes an affirmative response string and returns a
+                version of that string with any desired custom formatting applied. This
+                may be set to one of the class methods of `Color`, as a simple way to
+                emphasize the valid responses to the prompt.
+            quote_responses:
+                Whether to wrap each possible affirmative response in double quotes.
+                These quotes are added after `format_response()` is invoked on the
+                response (if provided). Defaults to `True`.
+
+        Returns:
+            A string prompting the user for an affirmative response.
+        """
+
         def get_display_response(response: str):
             if format_response:
                 response = format_response(response)
@@ -115,15 +192,6 @@ class Strings:
             if len(responses) < 3
             else f"{sep.join(responses[:-1])}{sep}{conj} {responses[-1]}"
         )
-
-    @classmethod
-    def default(cls) -> Strings:
-        return cls()
-
-    @classmethod
-    def compact(cls) -> Strings:
-        default_items = asdict(cls.default()).items()
-        return cls(**{key: _get_compact_value(value) for key, value in default_items})
 
 
 @overload
