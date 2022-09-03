d7dc3cc6d350a1d244c61975b47666712df77184
Nuz / Lovegood <nuztalgia@gmail.com>
2022-09-02 17:53:00 -0700

    Clarify docstrings+types and rearrange functions in Secret class.

--- botstrap/internal/secrets.py
+++ botstrap/internal/secrets.py
@@ -6,7 +6,7 @@ import re
 from base64 import urlsafe_b64encode
 from functools import partial
 from pathlib import Path
-from typing import Any, Callable, Final, cast
+from typing import Any, Callable, Final, Literal, TypeAlias, cast, get_args
 
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
@@ -14,12 +14,7 @@ from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
 from botstrap.internal.metadata import Metadata
 
-_MINIMUM_PASSWORD_LENGTH: Final[int] = 8
-
-_CONTENT_FILE: Final[str] = "content"
-_FERNET_FILE: Final[str] = "fernet"
-
-_KEY_FILES: Final[tuple[str, ...]] = (_CONTENT_FILE, _FERNET_FILE)
+_KeyQualifier: TypeAlias = Literal["content", "fernet"]
 
 
 class Secret:
@@ -33,14 +28,15 @@ class Secret:
         <div id="key-files"/>
         This class uses [Fernet][1] symmetric encryption from the [`cryptography`][2]
         package to encrypt and decrypt data, optionally with extra protection in the
-        form of a password. The encrypted data is guaranteed to be unreadable without
-        the **key** that was used to encrypt it. Fernet keys are uniquely generated
-        for each secret at encryption time.
+        form of a [password](./#passwords). The encrypted data is guaranteed to be
+        unreadable without the original **key** that was used to encrypt it.
+        Fernet keys are uniquely and randomly generated for each individual secret
+        at encryption time.
 
-        Each secret is therefore represented in the file system by two separate `.key`
-        files, both of which include the secret's `uid` in their file names. One of
-        the files contains the encrypted `content` of the secret, and the other file
-        contains the `fernet` key required to decrypt it. Each of these files is
+        A secret is therefore represented in the file system by two separate `.key`
+        files, both of which are named using the secret's `uid` (unique ID).
+        One of the files contains the encrypted `content` of the secret, and the other
+        file contains the `fernet` key required to decrypt it. Each of these files is
         useless without the other. :closed_lock_with_key:
 
     ??? info "Info - Fernet encryption with passwords"
@@ -54,10 +50,10 @@ class Secret:
         If a password is provided when a secret's
         [`write()`][botstrap.internal.Secret.write] method is invoked,
         the specified data will be encrypted using an algorithm based on [this][3]
-        reference from the Fernet documentation. The password will go through the
-        [`PBKDF2HMAC`][4] key derivation function in order to account for it in the
-        `fernet` key of the secret. The result of this procedure is that the password
-        will be required to "complete" the key **every time** the secret is decrypted.
+        reference implementation. The password will be run through the [`PBKDF2HMAC`][4]
+        key derivation function in order to obtain the Fernet key for the secret.
+        The result of this process is that the original password will be required to
+        "complete" the key **every time** the secret is decrypted.
 
     [1]:https://cryptography.io/en/latest/fernet/
     [2]:https://pypi.org/project/cryptography/
@@ -77,8 +73,8 @@ class Secret:
 
         Args:
             uid:
-                A unique string for identifying this secret. Will be used in the names
-                of the files containing this secret's data.
+                A unique string to identify this secret.
+                Will be used in the names of the files containing this secret's data.
             requires_password:
                 Whether a user-provided password is required in order to read and/or
                 write the data for this secret.
@@ -100,11 +96,46 @@ class Secret:
         if (not uid) or (not str(uid).isidentifier()):
             raise ValueError("Unique ID (uid) must be a valid non-empty identifier.")
 
+        def get_storage_directory() -> Path:
+            """Turns storage_directory into a valid Path, creating the dir if needed."""
+            nonlocal storage_directory
+
+            if not storage_directory:
+                storage_directory = Metadata.get_default_keys_dir()
+            elif isinstance(storage_directory, str):
+                storage_directory = Path(storage_directory)
+
+            if (storage_directory := storage_directory.resolve()).is_file():
+                raise ValueError(
+                    f'Expected a directory, but found a file: "{storage_directory}"'
+                )
+
+            storage_directory.mkdir(exist_ok=True)
+            return storage_directory
+
+        def get_validator() -> Callable[[str], bool]:
+            """Turns valid_pattern into a function that takes a str & returns a bool."""
+            nonlocal valid_pattern
+
+            if not valid_pattern:
+                valid_pattern = re.compile(".*", re.DOTALL)
+            elif isinstance(valid_pattern, str):
+                valid_pattern = re.compile(valid_pattern)
+
+            def validate_pattern(text: str) -> bool:
+                """Returns `True` if the given text fits the pattern for this secret."""
+                if isinstance(valid_pattern, re.Pattern):
+                    return valid_pattern.match(text) is not None
+                else:
+                    return bool(cast(Callable, valid_pattern)(text))
+
+            return validate_pattern
+
         self.uid: Final[str] = str(uid)
         self.requires_password: Final[bool] = requires_password
         self.display_name: Final[str] = display_name or uid
-        self.storage_directory: Final[Path] = _get_storage_directory(storage_directory)
-        self.validate: Final[Callable[[str], bool]] = _get_validator(valid_pattern)
+        self.storage_directory: Final[Path] = get_storage_directory()
+        self.validate: Final[Callable[[str], bool]] = get_validator()
 
     def __str__(self) -> str:
         """Returns a nicely-printable string representation of this secret."""
@@ -117,9 +148,9 @@ class Secret:
         This property will only return the `content` file path, as the `fernet` file is
         irrelevant outside of this class. The return value will be an instance of
         `pathlib.Path`, but it is **not** guaranteed to point to an existing file
-        (e.g. if this secret's data hasn't been created/saved yet, or has been deleted).
+        (e.g. if this secret hasn't been created/saved yet, or has been deleted).
         """
-        return self._get_key_file(_CONTENT_FILE)
+        return self._get_key_file("content")
 
     @property
     def min_pw_length(self) -> int:
@@ -129,7 +160,26 @@ class Secret:
         (chosen arbitrarily to try and balance security vs. convenience).
         For secrets that don't require a password, this will return `0`.
         """
-        return _MINIMUM_PASSWORD_LENGTH if self.requires_password else 0
+        return 8 if self.requires_password else 0
+
+    def clear(self) -> None:
+        """Deletes all files containing data related to this secret, if any exist.
+
+        This method **does not** scan the entire system to locate the files for a secret
+        - it only checks the [`storage_directory`][botstrap.internal.Secret.__init__]
+        that was specified upon instantiation.
+
+        ??? tip "Tip - Don't scramble your secrets!"
+            If a secret's [`.key`](./#key-files) files are renamed or moved out of their
+            original directory without a corresponding change to the `uid` and/or
+            `storage_directory` constructor parameters (or vice versa), then the secret
+            will behave as if there are no existing files associated with it.
+            Fortunately, this can easily be resolved by either moving the files back
+            into place or updating the constructor parameters in your code.
+        """
+        for qualifier in get_args(_KeyQualifier):
+            key_file = self._get_key_file(qualifier)
+            key_file.unlink(missing_ok=True)
 
     def read(self, password: str | None = None) -> str | None:
         """Returns the decrypted data from this secret's file if it exists and is valid.
@@ -180,28 +230,9 @@ class Secret:
             raise ValueError(f'Attempted to write invalid data for "{self.uid}".')
         self.file_path.write_bytes(self._get_fernet(password).encrypt(data.encode()))
 
-    def clear(self) -> None:
-        """Deletes all files containing data related to this secret, if any exist.
-
-        This method **does not** scan the entire system to locate the files for a secret
-        - it only checks the [`storage_directory`][botstrap.internal.Secret.__init__]
-        that was specified upon instantiation.
-
-        !!! tip "Tip - Don't scramble your secrets!"
-            If a secret's [`.key`](./#key-files) files are renamed or moved out of their
-            original directory without a corresponding change to the `uid` and/or
-            `storage_directory` constructor parameters (or vice versa), then the secret
-            will behave as if there are no existing files associated with it.
-            Fortunately, this can easily be resolved by either moving the files back
-            into place or updating the constructor parameters in your code.
-        """
-        for qualifier in _KEY_FILES:
-            key_file = self._get_key_file(qualifier)
-            key_file.unlink(missing_ok=True)
-
-    def _get_key_file(self, qualifier: str) -> Path:
+    def _get_key_file(self, qualifier: _KeyQualifier) -> Path:
         """Returns the `Path` to the qualified (content/fernet) file for this secret."""
-        if qualifier not in _KEY_FILES:
+        if qualifier not in get_args(_KeyQualifier):
             raise ValueError(f'Invalid key file qualifier: "{qualifier}"')
 
         if (file := self.storage_directory / f".{self.uid}.{qualifier}.key").is_dir():
@@ -221,7 +252,7 @@ class Secret:
                 raise ValueError(f"Password must be at least {length} characters long.")
 
         def get_extra_bytes(get_initial_bytes: Callable[[], bytes]) -> bytes:
-            if not (fernet_file := self._get_key_file(_FERNET_FILE)).is_file():
+            if not (fernet_file := self._get_key_file("fernet")).is_file():
                 fernet_file.write_bytes(get_initial_bytes())
             return fernet_file.read_bytes()
 
@@ -237,35 +268,3 @@ class Secret:
             key = get_extra_bytes(Fernet.generate_key)
 
         return Fernet(key)
-
-
-def _get_validator(
-    pattern: str | re.Pattern | Callable[[str], Any] | None
-) -> Callable[[str], bool]:
-    """Turns the `pattern` into a function that accepts a string and returns a bool."""
-    if not pattern:
-        pattern = re.compile(".*", re.DOTALL)
-    elif isinstance(pattern, str):
-        pattern = re.compile(pattern)
-
-    def validate_pattern(text: str) -> bool:
-        if isinstance(pattern, re.Pattern):
-            return pattern.match(text) is not None
-        else:
-            return bool(cast(Callable, pattern)(text))
-
-    return validate_pattern
-
-
-def _get_storage_directory(directory_path: str | Path | None) -> Path:
-    """Turns the `directory_path` into a usable `Path`, creating the dir if needed."""
-    if not directory_path:
-        directory_path = Metadata.get_default_keys_dir()
-    elif isinstance(directory_path, str):
-        directory_path = Path(directory_path)
-
-    if (directory_path := directory_path.resolve()).is_file():
-        raise ValueError(f'Expected a directory, but found a file: "{directory_path}"')
-
-    directory_path.mkdir(exist_ok=True)
-    return directory_path
