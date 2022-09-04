f014932d11145474306f0a61a9ad88f4fcb6b330
Nuz / Lovegood <nuztalgia@gmail.com>
2022-09-04 00:08:45 -0700

    Refactor/restructure global.js.

--- docs/javascripts/global.js
+++ docs/javascripts/global.js
@@ -1,77 +1,77 @@
-// Remove redundant docstrings and irrelevant comments in source code blocks.
-let quoteBlocks = document.querySelectorAll("details.quote");
-for (let i = 0; i < quoteBlocks.length; i++) {
-  const sourcePattern = /^Source code in <code>botstrap.*\.py<\/code>$/;
-  if (quoteBlocks[i].querySelector("summary").innerHTML.match(sourcePattern)) {
-    const lineSpans = quoteBlocks[i].querySelectorAll("pre code > *");
-    let inDocString = false;
-    for (let j = 0; j < lineSpans.length; j++) {
-      const lineText = lineSpans[j].textContent;
-      if (lineText.match(/ {4}"{3}(.*\.)?\n/)) {
-        inDocString = !inDocString;
-      } else if (inDocString || lineText.match(/ *# noinspection /)) {
-        lineSpans[j].remove();
-      }
+window.addEventListener("DOMContentLoaded", (event) => {
+  // Remove the "#" comment characters preceding annotations in code blocks.
+  for (const comment of document.querySelectorAll(".annotate code span.c1")) {
+    if (comment.innerHTML.match(/# .*?\(\d\)$/)) {
+      comment.innerHTML = comment.innerHTML.replace(/# .*?\(/, "(");
+    }
+  }
+  // Remove redundant docstrings and irrelevant comments in source code blocks.
+  const sourceCodeSummary = /^Source code in <code>botstrap.*\.py<\/code>$/;
+  for (const quote of document.querySelectorAll("details.quote")) {
+    if (quote.querySelector("summary").innerHTML.match(sourceCodeSummary)) {
+      cleanUpSourceCode(quote.querySelectorAll("pre code > span"));
+    }
+  }
+  // Clean up headings for all functions and remove parentheses for properties.
+  for (const docFunction of document.querySelectorAll(".doc-function")) {
+    removeDefaultArgs(docFunction);
+    removePropertyParens(docFunction);
+  }
+  // Run custom logic for specific pages if the current location matches up.
+  if (window.location.href.match(/\/(api|internal)\/$/)) {
+    handleReferencePage();
+  }
+  // Remove the title header anchor link on every page.
+  document.querySelector("h1 a.headerlink")?.remove();
+});
+
+/** Removes redundant docstrings & irrelevant comments in source code blocks. */
+function cleanUpSourceCode(lineSpans) {
+  let inDocString = false;
+  for (const lineSpan of lineSpans) {
+    if (lineSpan.textContent.match(/ {4}"{3}(.*\.)?\n/)) {
+      inDocString = !inDocString;
+    } else if (inDocString || lineSpan.textContent.match(/ *# noinspection /)) {
+      lineSpan.remove();
     }
   }
 }
 
-// Remove the "#" comment characters preceding annotations in code blocks.
-let annotations = document.querySelectorAll(".annotate code span.c1");
-for (let i = 0; i < annotations.length; i++) {
-  if (annotations[i].innerHTML.match(/# .*?\(\d\)$/)) {
-    annotations[i].innerHTML = annotations[i].innerHTML.replace(/# .*?\(/, "(");
-  }
-}
-
-// Remove parentheses for properties in page navigation links and headings.
-let docFunctions = document.querySelectorAll(".doc-function");
-for (let i = 0; i < docFunctions.length; i++) {
-  if (docFunctions[i].querySelector(".doc-label-property")) {
-    const pageNavLink = document.querySelector(
-      `.md-nav__link[href="#${docFunctions[i].querySelector("h2").id}"]`,
-    );
-    pageNavLink.innerHTML = pageNavLink.innerHTML.replace(/\(\)/, "");
-    const docHeading = docFunctions[i].querySelector(".doc-heading");
-    docHeading.querySelector("code > .p").remove();
-  }
-}
-
-// Remove default arguments in function signature headings.
-for (let i = 0; i < docFunctions.length; i++) {
-  const funcHeading = docFunctions[i].querySelector(".doc-heading code");
-  const defaultArgMatches = funcHeading.innerHTML.matchAll(
+/** Removes default arguments in function signature headings. */
+function removeDefaultArgs(docFunction) {
+  const heading = docFunction.querySelector(".doc-heading code");
+  for (const match of heading.innerHTML.matchAll(
     / ?<span class="o">=<\/span>.*?<span class="p">(\(\))?(,|\))<\/span>/g,
-  );
-  for (const match of defaultArgMatches) {
-    funcHeading.innerHTML = funcHeading.innerHTML.replace(
-      match[0],
-      `<span class="p">${match[2]}</span>`,
+  )) {
+    const replacement = `<span class="p">${match[2]}</span>`;
+    heading.innerHTML = heading.innerHTML.replace(match[0], replacement);
+  }
+}
+
+/** Removes parentheses for properties in page nav links and headings. */
+function removePropertyParens(docFunction) {
+  if (docFunction.querySelector(".doc-label-property")) {
+    const navLink = document.querySelector(
+      `.md-nav__link[href="#${docFunction.querySelector("h2").id}"]`,
     );
+    navLink.innerHTML = navLink.innerHTML.replace(/\(\)/, "");
+    docFunction.querySelector(".doc-heading code > .p")?.remove();
   }
 }
 
-// Remove the title header anchor link on every page.
-let titleHeaderLink = document.querySelector("h1 a.headerlink");
-if (titleHeaderLink) {
-  titleHeaderLink.remove();
-}
-
-// Remove all header anchor links on reference index pages.
-if (window.location.href.match(/\/(api|internal)\/$/)) {
-  const headerLinks = document.querySelectorAll(".md-typeset a.headerlink");
-  for (let i = 0; i < headerLinks.length; i++) {
-    headerLinks[i].remove();
+/** Execute special logic on "API Reference" and "Internal Reference" pages. */
+function handleReferencePage() {
+  // Remove all header anchor links.
+  for (const link of document.querySelectorAll(".md-typeset a.headerlink")) {
+    link.remove();
   }
-}
-
-// Follow the first detected link when a card in a clickable grid is clicked.
-let cardElements = document.querySelectorAll(".clickable.grid :is(.card, li)");
-for (let i = 0; i < cardElements.length; i++) {
-  cardElements[i].addEventListener("click", function (event) {
-    const cardTargetLink = event.target.querySelector("a");
-    if (cardTargetLink) {
-      window.location.href = cardTargetLink.getAttribute("href");
-    }
-  });
+  // Follow the first detected link when a card in a clickable grid is clicked.
+  for (const cardElement of document.querySelectorAll(
+    ".clickable.grid :is(.card, li)",
+  )) {
+    cardElement.addEventListener("click", function (event) {
+      const destination = event.target.querySelector("a")?.getAttribute("href");
+      window.location.href = destination ?? window.location.href;
+    });
+  }
 }
