65f140fb2070aac133ba053733469b59bd66e598
Nuz / Lovegood <nuztalgia@gmail.com>
2022-09-23 19:32:06 -0700

    Allow subcommands to define custom args in the standalone CLI.

--- botstrap/cli/main.py
+++ botstrap/cli/main.py
@@ -21,6 +21,7 @@ _ASCII_HEADER: Final[tuple[tuple[str, Callable[[str], str]], ...]] = (
     (r"                                  |_|    ", Color.cyan),
 )
 _NOTE_TEXT: Final[str] = "This is the standalone CLI. For the Python API, see the docs."
+_ARG_NAMES: Final[str] = "name_or_flags"
 _HELP_ARGS: Final[tuple[str, str]] = ("-h", "--help")
 _NO_COLORS_ARGS: Final[tuple[str, str]] = ("-n", "--no-colors")
 
@@ -35,21 +36,19 @@ class BotstrapCli(CliSession):
             enable_colors:
                 Whether the console output text should be colored.
         """
-        metadata = Metadata.get_package_info("botstrap")
-        colors = CliColors(primary=Color.pink) if enable_colors else CliColors.off()
-        super().__init__(name=cast(str, metadata["name"]), colors=colors)
+        super().__init__(
+            name=cast(str, (metadata := Metadata.get_package_info("botstrap"))["name"]),
+            colors=CliColors(primary=Color.pink) if enable_colors else CliColors.off(),
+        )
 
         self.parser: Final[ArgumentParser] = ArgumentParser(self.name, add_help=False)
         self._command_parsers: Final[Any] = self.parser.add_subparsers()
         self._help: Final[list[str]] = [
             self._build_header(f"version {metadata['version']}", enable_colors),
             f"\n  {metadata['summary']}",
-            colors.lowlight(f"  NOTE: {_NOTE_TEXT}"),
-            (
-                f"\n{colors.highlight('Usage:')}\n  {self.name} "
-                f"{colors.primary('<command>')} {colors.lowlight('[options]')}"
-            ),
-            colors.highlight("\nCommands:"),
+            self.colors.lowlight(f"  NOTE: {_NOTE_TEXT}"),
+            self._build_usage(),
+            self.colors.highlight("\nCommands:"),
         ]  # More lines of text will be appended to this list throughout this method.
 
         def get_project_url(label: str) -> str:
@@ -58,48 +57,92 @@ class BotstrapCli(CliSession):
 
         self._add_subcommand(
             "docs",
-            description="Open the Botstrap API documentation in your web browser.",
+            "Open the Botstrap API documentation in your web browser.",
             url=get_project_url("Documentation"),
         )
         self._add_subcommand(
             "repo",
-            description="Open the Botstrap repository on GitHub in your web browser.",
+            "Open the Botstrap repository on GitHub in your web browser.",
             url=get_project_url("Source Code"),
         )
         self._add_subcommand(
             "site",
-            description="Open the Botstrap website in your browser.",
+            "Open the Botstrap website in your browser.",
             url=metadata["home_page"],
         )
-        self._add_subcommand(
-            "help", description="Display this help message.", callback=self._print_help
+        self._add_subcommand("help", "Display this help message.", self._print_help)
+
+        self._add_general_options()
+        self.parser.set_defaults(callback=self._print_help, callback_keys=())
+
+    def _add_argument(
+        self,
+        target_parser: ArgumentParser,
+        target_help: list[str],
+        *arg_name_or_flags: str,
+        **arg_parameters: Any,
+    ) -> None:
+        """Adds the specified argument to the targeted parser and help text list."""
+        target_help.append(
+            f"  {', '.join(arg_name_or_flags).ljust(18)} "
+            f"{self.colors.lowlight(arg_parameters.pop('help', None))}"
         )
+        target_parser.add_argument(*arg_name_or_flags, **arg_parameters)
 
-        self._help.append(colors.highlight("\nGeneral Options:"))
-        self._add_option(*_HELP_ARGS, description="Display help.")
-        self._add_option(*_NO_COLORS_ARGS, description="Suppress colored output.")
-        self.parser.set_defaults(callback=self._print_help)
-
-    def _add_option(self, *flags: str, description: str) -> None:
-        """Adds a generally-available CLI option. Logic is implemented elsewhere."""
-        self.parser.add_argument(*flags, action="store_true")
-        description = self.colors.lowlight(description)
-        self._help.append(f"  {', '.join(flags).ljust(18)} {description}")
+    def _add_general_options(
+        self,
+        custom_parser: ArgumentParser | None = None,
+        custom_help: list[str] | None = None,
+    ) -> None:
+        """Adds the two "general options" (`-h` and `-n`) to the appropriate parser."""
+        target_parser = custom_parser or self.parser
+        target_help = custom_help or ((not custom_parser) and self._help) or []
+        target_help.append(self.colors.highlight("\nGeneral Options:"))
+        self._add_argument(
+            target_parser,
+            target_help,
+            *_HELP_ARGS,
+            help="Display help.",
+            action="store_const",
+            const=functools.partial(self._print_help, custom_help=custom_help),
+            dest="callback",
+        )
+        self._add_argument(
+            target_parser,
+            target_help,
+            *_NO_COLORS_ARGS,
+            help="Suppress colored output.",
+            action="store_true",
+        )
 
     def _add_subcommand(
         self,
         name: str,
         description: str,
         callback: Callable[..., Any] = webbrowser.open,
+        *callback_args: dict[str, Any],
         **callback_kwargs: Any,
     ) -> None:
-        """Adds a rudimentary CLI subcommand that simply invokes a callback function."""
+        """Adds a subcommand (with a callback & optional args of its own) to the CLI."""
         subparser = self._command_parsers.add_parser(name, add_help=False)
-        subparser.add_argument(
-            *_HELP_ARGS, action="store_const", const=self._print_help, dest="callback"
+        callback_keys = []
+
+        if callback_args:
+            custom_help = [f"  {description}", self.colors.highlight("\nCommand Args:")]
+            usage_keys = []
+            for arg_params in callback_args:
+                usage_keys.append((name_or_flags := arg_params.pop(_ARG_NAMES))[0])
+                callback_keys.append(name_or_flags[-1].strip("-"))
+                self._add_argument(subparser, custom_help, *name_or_flags, **arg_params)
+            custom_help.insert(0, f"{self._build_usage(name, *usage_keys)}\n")
+        else:
+            custom_help = []
+
+        subparser.set_defaults(
+            callback=functools.partial(callback, **callback_kwargs),
+            callback_keys=callback_keys,
         )
-        subparser.add_argument(*_NO_COLORS_ARGS, action="store_true")
-        subparser.set_defaults(callback=functools.partial(callback, **callback_kwargs))
+        self._add_general_options(subparser, custom_help)
         self._help.append(f"  {self.colors.primary(name.ljust(8))} {description}")
 
     def _build_header(self, version: str, enable_colors: bool) -> str:
@@ -113,12 +156,32 @@ class BotstrapCli(CliSession):
             last_line = last_line.center(width).replace(tail, last_color(tail))
         return "\n".join(line.center(width) for line in [*lines, last_line])
 
-    def _print_help(self) -> None:
-        """Prints a nicely-formatted help message for the standalone CLI."""
-        print("\n".join(self._help), end="\n\n")
+    def _build_usage(self, command: str = "", *extra_arg_names: str) -> str:
+        """Returns formatted usage info text for the command, or the CLI in general."""
+        label_separator = " " if command else "\n  "
+        component_strings = [
+            f"\n{self.colors.highlight('Usage:')}{label_separator}{self.name}",
+            self.colors.primary(command or "<command>"),
+            self.colors.lowlight("[options]"),
+            *[f"[{extra_arg_name}]" for extra_arg_name in extra_arg_names],
+        ]
+        return " ".join(component_strings)
+
+    def _print_help(self, custom_help: list[str] | None = None) -> None:
+        """Prints nicely-formatted help text for (a command in) the standalone CLI."""
+        print("\n".join(custom_help or self._help), end="\n\n")
 
 
 def main() -> None:
     """Acts as the primary entry point for (and executes) Botstrap's standalone CLI."""
-    cli = BotstrapCli(enable_colors=not any(arg in sys.argv for arg in _NO_COLORS_ARGS))
-    cli.parser.parse_args().callback()
+
+    def has_args(*args_to_check: str) -> bool:
+        """Returns True if any of the given args were specified on the command line."""
+        return any(arg in sys.argv for arg in args_to_check)
+
+    args = BotstrapCli(enable_colors=not has_args(*_NO_COLORS_ARGS)).parser.parse_args()
+
+    if (keys := args.callback_keys) and (not has_args(*_HELP_ARGS)):
+        args.callback(**{k: v for k, v in vars(args).items() if k in keys})
+    else:
+        args.callback()
